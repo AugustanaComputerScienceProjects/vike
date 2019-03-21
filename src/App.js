049dbf755d9555f61e4132fa817d14a640abc2d3
cannelljack <cannell.jack@gmail.com>
2019-03-21 14:15:11 -0500

    begginings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
 
 
 
@@ -10,9 +14,45 @@ class App extends Component {
 
      render() {
          return (
-             <div className='fullPage' style={{ backgroundColor: 'blue'}}>
+             <div className='fullPage' style={{ backgroundColor: 'blue' }}>
+                 <Grid
+                     container
+                     direction="row"
+                     justify="center"
+                     alignItems="center"
+                 >
+                     <Grid item xs={1}>
+                         <Paper> Pick a time:
+                             
+                             </Paper>
+                      
+                     </Grid>
+                     <Grid item xs={1}>
+                         <Paper>
+                         <TextField
+                             type="time"
+                             variant="filled"
+                             color='white'
+                             backgroundColor='white'
+                             />
+                         </Paper>
+                     </Grid>
 
-                 <button type="button" style={{ color: 'pink', padding: '80px' }}>Click</button>
+                         <Grid item xs={1}>
+                             <Paper>
+                         <TextField
+                             type="Date"
+                             variant="filled"
+                             color='white'
+                             backgroundColor='white'
+                             />
+                         </Paper>
+                     </Grid>
+
+                 </Grid>
+
+
+                
              </div>
 
     );
