bece8553675e3af4b9268454a80b8f555bebaac1
Thompson <brandonthompson15@augustana.edu>
2019-04-18 14:14:18 -0500

    First column for adding new events on the events page complete, no functionality

--- src/Events.js
+++ src/Events.js
@@ -1,21 +1,102 @@
 import React, { Component, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
+import {MuiPickersUtilsProvider, TimePicker, DatePicker} from 'material-ui-pickers';
+import MomentUtils from '@date-io/moment';
 import Paper from '@material-ui/core/Paper';
-import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+
+const testTags = [
+    'comedy',
+    'movie',
+    'food',
+    'raffle',
+    'caps',
+    'performance',
+];
 
 class Events extends Component {
+    state = {
+        tags: [],
+    }
 
-
+    handleChange = event => {
+        this.setState({tags: event.target.value});
+    };
 
     render() {
         return (
             <div>
-                <p>events </p>
+                <MuiPickersUtilsProvider utils={MomentUtils}>
+                <Grid container spacing={24}>
+                    <Grid item container direction="column" xs>
+                        <Grid item>
+                            <TextField
+                                    label="Event Title"
+                                    id="event-name"
+                                    margin="normal" />                        
+                        </Grid>
+                        <Grid item>
+                            <DatePicker
+                                margin="normal"
+                                lable="Date Picker"
+                                />
+                        </Grid>
+                        <Grid item>
+                            <TimePicker
+                                margin="normal"
+                                label="Time Picker"
+                                />
+                        </Grid>
+                        <Grid item container direction="row">
+                            <FormControl className="selects">
+                                <InputLabel htmlFor="select-multiple">Tags</InputLabel>
+                                <Select
+                                    multiple
+                                    displayEmpty
+                                    input={<Input id="select-multiple"/>}
+                                    value={this.state.tags}
+                                    onChange={this.handleChange}
+                                    variant='outlined'
+                                    style={{minWidth: '150px',maxWidth: '150px'}}
+                                    >
+                                    <MenuItem disabled value="">
+                                        <em>Select Tags</em>
+                                    </MenuItem>
+                                    {testTags.map(tag => (
+                                        <MenuItem key={tag} value={tag}>
+                                            {tag}
+                                        </MenuItem>
+                                    ))}    
+                                </Select>
+                            </FormControl>
+                            <Paper style={{marginLeft: '20px'}}>QR Code</Paper>
+                        </Grid>
+                        <Grid item>
+                            <TextField
+                                id="event-desc"
+                                label="Description"
+                                multiline
+                                rows="5"
+                                margin="normal"
+                                variant="outlined"
+                                />
+                        </Grid>
+                        <Grid item>
+                            <Button variant="contained"
+                                className="create-event">
+                                Submit Event    
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                </MuiPickersUtilsProvider>
             </div>
 
 
