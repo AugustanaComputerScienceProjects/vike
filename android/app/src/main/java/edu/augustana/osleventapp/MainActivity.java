e286615bb56468e38aeb1f66d94b007cb35c4cfd
Hung Tran [I] <hungtra@tesla.com>
2023-05-03 16:42:29 -0500

    make android works

--- android/app/src/main/java/edu/augustana/osleventapp/MainActivity.java
+++ android/app/src/main/java/edu/augustana/osleventapp/MainActivity.java
@@ -1,23 +1,26 @@
 package edu.augustana.osleventapp;
 
 import com.facebook.react.ReactActivity;
-
-import com.facebook.react.ReactActivityDelegate; // <- react-native-bootsplash import
+import com.facebook.react.ReactRootView;
+import com.facebook.react.ReactActivityDelegate;
+import android.os.Bundle;
 import com.zoontek.rnbootsplash.RNBootSplash; // <- react-native-bootsplash import
 
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this); // <- initialize the splash screen
+    super.onCreate(savedInstanceState); // or super.onCreate(null) with react-native-screens
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
   @Override
-  protected String getMainComponentName() {
-    @Override
-      protected void loadApp(String appKey) {
-        RNBootSplash.init(MainActivity.this); // <- initialize the splash screen
-        super.loadApp(appKey);
-      }
+    protected String getMainComponentName() {
+      return "OSLEventApp";
   }
 }
