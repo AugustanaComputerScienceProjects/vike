696ce1684819e17384b5df1731557060bc76ae64
tthuwng <tronghung199@gmail.com>
2023-09-15 22:18:05 -0500

    add google sign in

--- App.js
+++ App.js
@@ -1,13 +1,58 @@
 import auth from '@react-native-firebase/auth';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
+GoogleSignin.configure({
+  webClientId:
+    '559059413195-kc0din44sr01g9opm9nnnb3ve06mvrcq.apps.googleusercontent.com',
+  hostedDomain: 'augustana.edu',
+});
+
 export default function App() {
   console.log(auth().currentUser);
+  const signIn = async () => {
+    try {
+      // Get the users ID token
+      const {idToken} = await GoogleSignin.signIn();
+
+      // Create a Google credential with the token
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+      // Sign-in the user with the credential
+      return auth()
+        .signInWithCredential(googleCredential)
+        .then(user => {
+          console.log('user', user);
+        });
+    } catch (error) {
+      console.log('error', error.code, error.message);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on Vike!</Text>
+      <GoogleSigninButton
+        style={{width: 192, height: 48}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={signIn}
+        // disabled={this.state.isSigninInProgress}
+      />
       <StatusBar style="auto" />
     </View>
   );
